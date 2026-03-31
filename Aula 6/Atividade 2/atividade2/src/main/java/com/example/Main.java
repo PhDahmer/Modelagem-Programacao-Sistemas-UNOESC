e0768a2c3094716934a416e61efd25021465b74b
PhDahmer <pedrohdahmer@proton.me>
2026-03-31 20:13:33 -0300

    feat: Aula 7 - Ajuste atividade2

--- Aula 6/Atividade 2/atividade2/src/main/java/com/example/Main.java
+++ Aula 6/Atividade 2/atividade2/src/main/java/com/example/Main.java
@@ -1,28 +1,47 @@
 package com.example;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         
+        ArrayList<Funcionario> funcionarios = new ArrayList<>();
+
         Docente docente1 = new Docente("Alice", 50.0, 160, "Doutorado", 3);
         Docente docente2 = new Docente("Charlie", 40.0, 160, "Mestrado", 2);
         Docente docente3 = new Docente("Eve", 30.0, 160, "Graduação", 1);
         Tecnico tecnico1 = new Tecnico("Bob", 30.0, 160, "Suporte Técnico", 20);
         Tecnico tecnico2 = new Tecnico("Dave", 25.0, 160, "Manutenção", 10);
 
+        funcionarios.add(docente1);
+        funcionarios.add(docente2);
+        funcionarios.add(docente3);
+        funcionarios.add(tecnico1);
+        funcionarios.add(tecnico2);
+
         System.out.println("");
         System.out.println("=-=-=-=-=-=-= Fundacao Pedro Dahmer de Ensino e Pesquisa em Humanos =-=-=-=-=-=-=");
         System.out.println("");
         System.out.println("Funcionários:");
         System.out.println("");
-        System.out.println("" + docente1.getNome() + " - Titulação: " + docente1.getTitulacao() + ", Nível: " + docente1.getNivel() + ", Salário: " + docente1.calcularSalario());
-        System.out.println("-");
-        System.out.println("" + docente2.getNome() + " - Titulação: " + docente2.getTitulacao() + ", Nível: " + docente2.getNivel() + ", Salário: " + docente2.calcularSalario());
-        System.out.println("-");
-        System.out.println("" + docente3.getNome() + " - Titulação: " + docente3.getTitulacao() + ", Nível: " + docente3.getNivel() + ", Salário: " + docente3.calcularSalario());
-        System.out.println("-");
-        System.out.println("" + tecnico1.getNome() + " - Função: " + tecnico1.getFuncao() + ", Horas Extras: " + tecnico1.getHorasExtras() + ", Salário: " + tecnico1.calcularSalario());
-        System.out.println("-");
-        System.out.println("" + tecnico2.getNome() + " - Função: " + tecnico2.getFuncao() + ", Horas Extras: " + tecnico2.getHorasExtras() + ", Salário: " + tecnico2.calcularSalario());
+
+        funcionarios.forEach(funcionario -> {
+            System.out.println("Nome: " + funcionario.getNome());
+            if (funcionario instanceof Docente) {
+                Docente docente = (Docente) funcionario;
+                System.out.println("Cargo: Docente");
+                System.out.println("Titulação: " + docente.getTitulacao());
+                System.out.println("Nível de Ensino: " + docente.getNivel());
+            } else if (funcionario instanceof Tecnico) {
+                Tecnico tecnico = (Tecnico) funcionario;
+                System.out.println("Cargo: Técnico");
+                System.out.println("Função: " + tecnico.getFuncao());
+                System.out.println("Horas Extras: " + (tecnico.getHorasExtras()));
+            }
+            System.out.println("Salário: R$ " + String.format("%.2f", funcionario.calcularSalario()));
+            System.out.println("");
+        });
+
         System.out.println("");
         System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
         System.out.println("");
